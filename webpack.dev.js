62be173cc28b1ac8fe7e53cbc009a08a0cda9107
horumiko <79800115+horumiko@users.noreply.github.com>
2022-05-10 01:15:19 +0300

    Add files via upload

---
+++ webpack.dev.js
@@ -0,0 +1,26 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+const path = require('path');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ],
+      },
+    ],
+  },
+  plugins: [new ESLintPlugin()],
+});
